a21258802990999dce673121a74212c92af8e788
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-09-19 19:25:55 +0300

    19.09

---
+++ BusinessLayer/Abstract/IFoodService.cs
@@ -0,0 +1,18 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+   public interface IFoodService
+    {
+        void Add(Food food);
+        void Delete(Food food);
+        void Update(Food food);
+        List<Food> GetAll();
+        Food GetById(int id);
+    }
+}
---
+++ BusinessLayer/Concrete/FoodManager.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class FoodManager : IFoodService
+    {
+        IFoodDal _foodDal;
+
+        public FoodManager(IFoodDal foodDal)
+        {
+            _foodDal = foodDal;
+        }
+
+        public void Add(Food food)
+        {
+            _foodDal.insert(food);
+        }
+
+        public void Delete(Food food)
+        {
+            _foodDal.Delete(food);
+        }
+
+        public List<Food> GetAll()
+        {
+            return _foodDal.GetAll();
+        }
+
+        public Food GetById(int id)
+        {
+            return _foodDal.GetById(id);
+        }
+
+        public void Update(Food food)
+        {
+            _foodDal.Update(food);
+        }
+    }
+}
