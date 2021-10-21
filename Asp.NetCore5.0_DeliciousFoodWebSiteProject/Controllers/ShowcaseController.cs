684d0cf1089efe69dde1f9abb8b94fda42efec18
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-10-21 19:25:14 +0300

    21.10 / Added Showcase

---
+++ Asp.NetCore5.0_DeliciousFoodWebSiteProject/Controllers/ShowcaseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DeliciousFoodWebSiteProject.Controllers
+{
+    [AllowAnonymous]
+    public class ShowcaseController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore5.0_DeliciousFoodWebSiteProject/ViewComponents/Category/CategoryGetList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DeliciousFoodWebSiteProject.ViewComponents.Category
+{
+    public class CategoryGetList:ViewComponent
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryRepository());
+        public IViewComponentResult Invoke()
+        {
+            var value = categoryManager.GetAll();
+            return View(value);
+        }
+    }
+}
