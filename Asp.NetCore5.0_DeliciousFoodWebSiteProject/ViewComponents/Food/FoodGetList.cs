4d2903a08a337accb47aa24a8998c24995a26ffa
Mine Topcuoglu <77547699+minetpcuglu@users.noreply.github.com>
2021-10-21 21:25:04 +0300

    21.10 / FoodGetList VC Added

---
+++ Asp.NetCore5.0_DeliciousFoodWebSiteProject/ViewComponents/Food/FoodGetList.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCore5._0_DeliciousFoodWebSiteProject.ViewComponents.Food
+{
+    public class FoodGetList:ViewComponent
+    {
+        FoodManager foodManager = new FoodManager(new EfFoodRepository());
+        public IViewComponentResult Invoke()
+        {
+            var value =foodManager.GetAll();
+            return View(value);
+        }
+    }
+}
